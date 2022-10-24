f86209d59d74e69d727d5976607006a721ab4e18
JJ Wilson <32716648+SonJJ74@users.noreply.github.com>
2022-10-24 20:29:35 +0100

    Base singleton code implementation

---
+++ Singleton/ChocolateBoiler/ChocolateBoiler.cs
@@ -0,0 +1,52 @@
+﻿namespace Singleton.ChocolateBoiler
+{
+    public class ChocolateBoiler
+    {
+        private static ChocolateBoiler? _uniqueInstance;
+
+        private ChocolateBoiler()
+        {
+            IsEmpty = true;
+            HasBoiled = false;
+        }
+
+        public bool IsEmpty { get; private set; }
+
+        public bool HasBoiled { get; private set; }
+
+        public static ChocolateBoiler GetInstance()
+        {
+            _uniqueInstance ??= new ChocolateBoiler();
+
+            return _uniqueInstance;
+        }
+
+        public void Fill()
+        {
+            if (IsEmpty)
+            {
+                IsEmpty = false;
+                HasBoiled = false;
+                //todo: add code to fill the boiler with a milk/chocolate mixture
+            }
+        }
+
+        public void Boil()
+        {
+            if (!IsEmpty && !HasBoiled)
+            {
+                //todo: bring the contents to a boil
+                HasBoiled = true;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!IsEmpty && HasBoiled)
+            {
+                //todo: drain the boiled milk and chocolate
+                IsEmpty = true;
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton.ChocolateBoiler;
+
+Console.WriteLine("Hello, Singleton Pattern!");
+
+var boiler = ChocolateBoiler.GetInstance();
+
+boiler.Fill();
+boiler.Boil();
+boiler.Drain();
+
+//This boiler should be the first boiler
+var secondBoiler = ChocolateBoiler.GetInstance();
+var isEmpty = boiler.IsEmpty;
+var hasBoiled = boiler.HasBoiled;
